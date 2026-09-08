4a352949d0d5c1aef1bce82e85d03df2032bd8ba
Mansi2007275 <yadavmansi0111207@gmail.com>
2026-09-08 21:26:31 +0530

    feat(florestad): add flags to disable RPC, Electrum, and ZMQ servers
    Add --disable-rpc, --disable-electrum, and --disable-zmq CLI flags to disable the corresponding servers at runtime without a rebuild.
    
    - Add disable_rpc/disable_electrum/disable_zmq fields to CLI and Config
    - Gate RPC, Electrum, and ZMQ server startup on these flags
    - Simplify start_electrum's cfilters parameter (stable API, no cfg split)
    - Move the disable_electrum check to the call site instead of inside start_electrum
    - Add Python integration tests with a wait_on_socket helper in the electrum test framework

--- crates/floresta-node/src/florestad.rs
+++ crates/floresta-node/src/florestad.rs
@@ -225,6 +225,15 @@ pub struct Config {
     /// and won't affect the node's operation. You may notice that this will take a lot of CPU
     /// and bandwidth to run.
     pub backfill: bool,
+
+    /// Whether the JSON-RPC server should be disabled
+    pub disable_rpc: bool,
+
+    /// Whether the Electrum server should be disabled
+    pub disable_electrum: bool,
+
+    /// Whether the ZMQ server should be disabled
+    pub disable_zmq: bool,
 }
 
 impl Config {
@@ -259,6 +268,9 @@ impl Config {
             tls_cert_path: None,
             allow_v1_fallback: false,
             backfill: false,
+            disable_rpc: false,
+            disable_electrum: false,
+            disable_zmq: false,
         }
     }
 }
@@ -457,18 +469,20 @@ impl Florestad {
         // ZMQ
         #[cfg(feature = "zmq-server")]
         {
-            info!("Starting ZMQ server");
-            if let Ok(zserver) = ZMQServer::new(
-                self.config
-                    .zmq_address
-                    .as_ref()
-                    .unwrap_or(&"tcp://127.0.0.1:5150".to_string()),
-            ) {
-                blockchain_state.subscribe(Arc::new(zserver));
-                info!("Done!");
-            } else {
-                error!("Could not create zmq server, skipping");
-            };
+            if !self.config.disable_zmq {
+                info!("Starting ZMQ server");
+                if let Ok(zserver) = ZMQServer::new(
+                    self.config
+                        .zmq_address
+                        .as_ref()
+                        .unwrap_or(&"tcp://127.0.0.1:5150".to_string()),
+                ) {
+                    blockchain_state.subscribe(Arc::new(zserver));
+                    info!("Done!");
+                } else {
+                    error!("Could not create zmq server, skipping");
+                };
+            }
         }
 
         info!("Starting server");
@@ -477,37 +491,52 @@ impl Florestad {
         // JSON-RPC
         #[cfg(feature = "json-rpc")]
         {
-            let server = tokio::spawn(json_rpc::server::RpcImpl::create(
-                blockchain_state.clone(),
-                wallet.clone(),
-                chain_provider.get_handle(),
-                self.stop_signal.clone(),
-                self.config.network,
-                cfilters.clone(),
-                self.config
-                    .json_rpc_address
-                    .as_ref()
-                    .map(|x| Self::resolve_hostname(x, self.config.network.default_rpc_port()))
-                    .transpose()?,
-                datadir.join("debug.log"),
-                self.config.user_agent.clone(),
-                proxy,
-                !self.config.allow_v1_fallback,
-            ));
+            if !self.config.disable_rpc {
+                let server = tokio::spawn(json_rpc::server::RpcImpl::create(
+                    blockchain_state.clone(),
+                    wallet.clone(),
+                    chain_provider.get_handle(),
+                    self.stop_signal.clone(),
+                    self.config.network,
+                    cfilters.clone(),
+                    self.config
+                        .json_rpc_address
+                        .as_ref()
+                        .map(|x| Self::resolve_hostname(x, self.config.network.default_rpc_port()))
+                        .transpose()?,
+                    datadir.join("debug.log"),
+                    self.config.user_agent.clone(),
+                    proxy,
+                    !self.config.allow_v1_fallback,
+                ));
 
-            if self.json_rpc.set(server).is_err() {
-                core::panic!("We should be the first one setting this");
+                if self.json_rpc.set(server).is_err() {
+                    core::panic!("We should be the first one setting this");
+                }
             }
         }
 
-        self.start_electrum(
-            wallet,
-            blockchain_state,
-            cfilters,
-            chain_provider.get_handle(),
-            datadir,
-        )
-        .await?;
+        if !self.config.disable_electrum {
+            #[cfg(feature = "compact-filters")]
+            self.start_electrum(
+                wallet,
+                blockchain_state,
+                cfilters,
+                chain_provider.get_handle(),
+                datadir,
+            )
+            .await?;
+
+            #[cfg(not(feature = "compact-filters"))]
+            self.start_electrum(
+                wallet,
+                blockchain_state,
+                None,
+                chain_provider.get_handle(),
+                datadir,
+            )
+            .await?;
+        }
 
         let (sender, receiver) = tokio::sync::oneshot::channel();
 
@@ -548,14 +577,14 @@ impl Florestad {
         wallet: Arc<AddressCache<KvDatabase>>,
         blockchain_state: Arc<ChainState<ChainStore>>,
         #[cfg(feature = "compact-filters")] cfilters: Option<Arc<NetworkFilters<FlatFiltersStore>>>,
-        #[cfg(not(feature = "compact-filters"))] cfilters: Option<()>,
+        #[cfg(not(feature = "compact-filters"))] _cfilters: Option<()>,
         node_handle: floresta_wire::node_handle::NodeHandle,
         datadir: &Path,
     ) -> Result<(), FlorestadError> {
+        // Electrum Server configuration.
+
         #[cfg(not(feature = "compact-filters"))]
         let cfilters = None;
-
-        // Electrum Server configuration.
 
         // Instantiate the Electrum Server.
         let electrum_server = ElectrumServer::new(wallet, blockchain_state, cfilters, node_handle)
